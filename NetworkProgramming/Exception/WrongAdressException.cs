f340a368f07a2e402e68bb4204eb5e1e33bd4e7e
tahi_ri96 <tahi_ri96@STHQ0127-05>
2021-12-20 13:36:04 +0400

    Add project files.

---
+++ NetworkProgramming/Exception/WrongAdressException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkProgramming.Exception1
+{
+    public class WrongAdressException:Exception
+    {
+        public WrongAdressException(string message):base(message)
+        {
+
+        }
+    }
+}
---
+++ NetworkProgramming/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,80 @@
+﻿using HtmlAgilityPack;
+using NetworkProgramming.Commands;
+using NetworkProgramming.Exception1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NetworkProgramming.ViewModel
+{
+    public class MainWindowViewModel:BaseViewModel
+    {
+        public string urlAddress { get; set; }
+
+
+
+        public RelayCommand SearchBtnClick { get; set; }
+        public MainWindowViewModel(MainWindow mainWindow)
+        {
+            SearchBtnClick = new RelayCommand((sender) =>
+            {
+
+                try
+                {
+                    urlAddress = mainWindow.AdressName.Text;
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    StreamReader readStream = null;
+
+
+
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+
+                        var doc = new HtmlWeb().Load(urlAddress);
+                        var linkTags = doc.DocumentNode.Descendants("link");
+                        var linkedPages = doc.DocumentNode.Descendants("a")
+                        .Select(a => a.GetAttributeValue("href", null))
+                        .Where(u => !String.IsNullOrEmpty(u));
+                        string[] arr = linkedPages.ToArray();
+                        if (arr != null)
+                        {
+                            mainWindow.Adress.Header = mainWindow.AdressName.Text;
+                            mainWindow.one.Header = arr[0];
+                            mainWindow.two.Header = arr[1];
+                            mainWindow.three.Header = arr[2];
+                            mainWindow.four.Header = arr[3];
+                            mainWindow.five.Header = arr[4];
+                            mainWindow.six.Header = arr[5];
+                            mainWindow.Seven.Header = arr[6];
+                        }
+                        else
+                        {
+                            throw new WrongAdressException("Adress Not Found! Try Again");
+                        }
+
+
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+
+
+
+
+            });
+
+
+
+        }
+    }
+}
